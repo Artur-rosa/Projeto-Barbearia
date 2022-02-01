edb8e71805f5d449f9616962f16dabad9d809007
Artur Lopes Rosa <artur_lrosa@hotmail.com>
2022-02-01 18:27:57 -0300

    Add files via upload

---
+++ Projeto Barbearia/teste.js
@@ -0,0 +1,34 @@
+
+/*
+console.log("ola mundo");
+
+const idade = 26;
+console.log(idade);
+console.log("ano" + 2021);
+console.log("2"+"2");
+
+console.log(parseInt("2") + parseInt("2"));
+const nome = 'Artur';
+console.log(nome);
+*/
+
+let idade =22;
+
+idade = idade +3; 
+console.log(idade);
+
+//listas las
+const listaDeDestinos = new Array(
+    "salvador",
+    "São paulo",
+    "Rio de Janeiro"
+);
+
+console.log(listaDeDestinos);
+ listaDeDestinos.push("porto alegre");
+ console.log(listaDeDestinos);
+
+ listaDeDestinos.splice(1,1);
+ console.log(listaDeDestinos[1]);
+
+ 
